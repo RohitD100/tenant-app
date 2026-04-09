4ee40bb4e0ae838ba80fa9179bd346d888078519
rohit <rohitdorage3@gmail.com>
2026-04-09 16:00:35 +0530

    feat: add useUser hook for user context management and update AppBar to conditionally render menu icon

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -5,25 +5,33 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useUser } from "../hooks/useUser";
 
 export default function ButtonAppBar() {
+  const { user } = useUser();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          {user && (
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          )}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Welcome!
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Signup</Button>
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
