ff33194307e3b7f63cb2f01a997dd3db199b40f7
rohit <rohitdorage3@gmail.com>
2026-04-10 20:32:01 +0530

    fix: update import path for IUser type in user API module

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { IUser } from "../pages/UsersManagement";
+import type { IUser } from "../types/user";
 import API from "./axios";
 
 /**
